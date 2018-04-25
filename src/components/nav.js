9f64766bd9f7f93a731e0673b084e327dca312b4
Theo <theoleanse@gmail.com>
2018-04-25 17:56:52 +0100

    Format code with prettier (#7)

--- src/components/nav.js
+++ src/components/nav.js
@@ -2,16 +2,36 @@ import { h } from 'preact';
 import { Checkbox } from './checkbox';
 import { uniqueArray } from '../lib/utils';
 
-export const Nav = ({ posts, tags, categories, selectTag, deselectTag }) =>
-    <nav class="w-third dib bg-white br relative">
-        <div class="flex flex-wrap">
-            { uniqueArray(posts.map(({date}) => new Date(date).getFullYear()))
-                .map(date => <Checkbox value={date} selectTag={selectTag} deselectTag={deselectTag} />) }
-        </div>
-        <div class="js-categories-container flex flex-wrap">
-            { categories.map(({name}) => <Checkbox value={name} selectTag={selectTag} deselectTag={deselectTag} />) }
-        </div>
-        <div class="js-tags-container flex flex-wrap">
-            { tags.map(({name}) => <Checkbox value={name} selectTag={selectTag} deselectTag={deselectTag} />) }
-        </div>
-    </nav>
+export const Nav = ({ posts, tags, categories, selectTag, deselectTag }) => (
+	<nav class="w-third dib bg-white br relative">
+		<div class="flex flex-wrap">
+			{uniqueArray(
+				posts.map(({ date }) => new Date(date).getFullYear())
+			).map(date => (
+				<Checkbox
+					value={date}
+					selectTag={selectTag}
+					deselectTag={deselectTag}
+				/>
+			))}
+		</div>
+		<div class="js-categories-container flex flex-wrap">
+			{categories.map(({ name }) => (
+				<Checkbox
+					value={name}
+					selectTag={selectTag}
+					deselectTag={deselectTag}
+				/>
+			))}
+		</div>
+		<div class="js-tags-container flex flex-wrap">
+			{tags.map(({ name }) => (
+				<Checkbox
+					value={name}
+					selectTag={selectTag}
+					deselectTag={deselectTag}
+				/>
+			))}
+		</div>
+	</nav>
+);
--- src/index.js
+++ src/index.js
@@ -1,16 +1,19 @@
 import { h, render } from 'preact';
 import { App } from './components/app';
 
-const fetchContent = type => fetch(`https://public-api.wordpress.com/wp/v2/sites/commonstudy.wordpress.com/${type}?per_page=100`)
-	.then(res => res.json())
+const fetchContent = type =>
+	fetch(
+		`https://public-api.wordpress.com/wp/v2/sites/commonstudy.wordpress.com/${type}?per_page=100`
+	).then(res => res.json());
 
-export const content = () => Promise.all([
-	fetchContent('posts'),
-	fetchContent('tags'),
-	fetchContent('categories')
-]);
+export const content = () =>
+	Promise.all([
+		fetchContent('posts'),
+		fetchContent('tags'),
+		fetchContent('categories')
+	]);
 
 (async () => {
-	const [ posts, tags, categories ] = await content();
-  render(<App {...{ posts, tags, categories }} />, document.body);
-})()
+	const [posts, tags, categories] = await content();
+	render(<App {...{ posts, tags, categories }} />, document.body);
+})();
