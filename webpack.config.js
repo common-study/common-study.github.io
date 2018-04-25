f54ddb5ffdfd0801a60a0266b84ea64d0ef9678e
theoleanse <theoleanse@gmail.com>
2018-04-25 17:37:09 +0100

    decko

--- webpack.config.js
+++ webpack.config.js
@@ -21,7 +21,8 @@ module.exports = {
 				options: {
 					presets: ['env'],
 					plugins: [
-						['transform-react-jsx', { pragma: 'h' }]
+						['transform-react-jsx', { pragma: 'h' }],
+						'transform-decorators-legacy'
 					]
 				}
 			}
